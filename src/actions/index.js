f163e10427b052598f27441f85e8343b3fe8f566
Frankzhac <Frank.hemle@gmail.com>
2019-05-23 09:31:02 -0400

    imported axios and created 3 actions types

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,12 @@
 // we'll need axios
+import axios from "axios";
 
 // we'll need to create 3 different action types here.
+export const FETCHING_CHARACTERS = "FETCHING_CHARACTERS";
+export const FETCHING_CHARACTERS_SUCCESS = "FETCHING_CHARACTERS_SUCCESS";
+export const FETCHING_CHARACTERS_FAILURE = "FETCHING_CHARACTERS_FAILURE";
 // one for fetching, one for success and one for failure
+
 
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
