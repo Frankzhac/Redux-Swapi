ac041af05a5fc985f0c15ce36688816f7da5abd1
Frankzhac <Frank.hemle@gmail.com>
2019-05-23 09:27:05 -0400

    actions fetching success and failure

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -11,6 +11,17 @@ export const charsReducer = (state = initialState, action) => {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case FETCHING_CHARACTERS:
+      return { ...state, fetching: true };
+    case FETCHING_CHARACTERS_FAILURE:
+      return { ...state, fetching: false, error: action.payload };
+    case FETCHING_CHARACTERS_SUCCESS:
+      console.log(action.payload);
+      return {
+        ...state,
+        characters: [...state.characters, ...action.payload],
+        fetching: false
+      };
     default:
       return state;
   }
